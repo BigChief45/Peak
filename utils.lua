8ed2a7bbe19aa6bb62aefad28e2ed63f10976163
Andrés Álvarez <andresalvarez353@gmail.com>
2017-07-25 15:19:08 +0800

    Add resetFont() util

--- utils.lua
+++ utils.lua
@@ -2,6 +2,10 @@ function resetColor()
 	love.graphics.setColor(255, 255, 255)
 end
 
+function resetFont()
+  love.graphics.setNewFont(15)
+end
+
 function randomBool()
   return (math.random(1,2) == 2)
 end
