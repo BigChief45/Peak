6842965fe6fa51ff4fd05b5a95ad8f59f8d2ef4b
Andrés Álvarez <andresalvarez353@gmail.com>
2017-07-25 11:26:50 +0800

    Implement initial main menu GUI using SUIT library

--- menu/menu.lua
+++ menu/menu.lua
@@ -1,12 +1,20 @@
 require 'menu/menu_button'
+Suit = require 'lib/suit'
 
 local Menu = Game:addState('Menu')
 
 local BG_COLOR = {1, 184, 252}
 
+-- Button colors
+Suit.theme.color = {
+    normal  = { bg = {255, 255, 255}, fg = {0, 0, 0 } },
+    hovered = { bg = {255, 165, 0}, fg = {255, 255, 255} },
+    active  = { bg = {255, 165, 0}, fg = {225, 225, 225} }
+}
+
 local GAMES = {
-  { 'Memory Sweep', '' },
-  { 'Navigation', '' }
+  'Memory Sweep',
+  'Navigation'
 }
 
 function Menu:enteredState()
@@ -15,22 +23,22 @@ function Menu:enteredState()
   self.gameButtons = {}
 end
 
-function Menu:draw()
-  for y=1, #GAMES do
-    local row = GAMES[y]
-    for x=1, #row do
-      local gameButton = MenuButton:new(row[1], nil, x, y)
-      table.insert(self.gameButtons, gameButton)
+function Menu:update(dt)
+  Suit.layout:reset(250, 100)
+  Suit.layout:padding(10, 10)
 
-      gameButton:draw()
+  for i=1, #GAMES do
+    if Suit.Button(GAMES[i], Suit.layout:row(300, 80)).hit then
+      self:gotoState('MemorySweep', math.random(3, 5), math.random(3, 5))
     end
   end
 end
 
+function Menu:draw()
+  Suit.draw()
+end
+
 function Menu:keypressed(key, code)
-  if key == 'space' then
-    self:gotoState('MemorySweep', math.random(3, 5), math.random(3, 5))
-  end
 end
 
 function Menu:mousepressed(x, y, button, isTouch)
